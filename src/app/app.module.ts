b15375305162ad5ae15b7791c057773790e8f171
dunqan <dunqan@w9.pl>
2021-03-03 15:43:13 +0100

    Classic spartacus 3.1 installed with schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {translationChunksConfig, translations} from '@spartacus/assets';
+import {B2cStorefrontModule} from '@spartacus/storefront';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://spartacus-devci7672.eastus.cloudapp.azure.com:9002'
+        }
+      },
+      context: {
+        baseSite: ['electronics-spa'],
+        currency: ['USD'],
+        language: ['en'],
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      features: {
+        level: '3.1'
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
